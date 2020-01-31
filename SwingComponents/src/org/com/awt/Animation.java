c08f9b2661331bd1b90a95cc9c82cc87fb5ac2a6
chetan <chetan@LAPTOP-80E498JG>
2020-01-31 17:26:14 +0530

    initial commit

---
+++ SwingComponents/src/org/com/awt/Animation.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.com.awt;
+
+/**
+ * @author chetan
+ *
+ */
+public class Animation extends java.applet.Applet implements Runnable {
+
+	private static final long serialVersionUID = -6910058691087097758L;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
